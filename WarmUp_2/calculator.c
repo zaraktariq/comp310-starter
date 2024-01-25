a25c36320ecca6f3355b8e67e21225e42e61ae88
Zarak Tariq <zaraktariq7@gmail.com>
2024-01-25 18:29:22 +0000

    calculator implementation using scanf and argc/v

--- WarmUp_2/calculator.c
+++ WarmUp_2/calculator.c
@@ -2,8 +2,19 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Please input the first number: ");
-    double num1 = scanf(("%lf"), &num1);
-    double num2;
 
+    float num1;
+    float num2;
+
+    printf("Please input the first number: ");
+    // scan formatted("Expect a float", saved to the memory position of num1);
+    scanf(("%f"), &num1);
+    printf("Please input the second number: ");
+    scanf(("%f"), &num2);
+    
+    // math operations
+    printf("%f + %f = %f\n", num1, num2, num1+num2);
+    printf("%f - %f = %f\n", num1, num2, num1-num2);
+    printf("%f / %f = %f\n", num1, num2, num1/num2);
+    printf("%f * %f = %f\n", num1, num2, num1*num2);
 }
---
+++ WarmUp_2/calculator_with_args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+    if(argc < 3) {
+        printf("this program requires 2 inputs\n");
+        return EXIT_FAILURE;
+    } else {
+        int val1 = strtol(argv[1], NULL, 10);
+        int val2 = strtol(argv[2], NULL, 10);
+
+        printf("%d + %d = %d\n", val1, val2, val1 + val2);
+        printf("%d - %d = %d\n", val1, val2, val1 - val2);
+        printf("%d / %d = %d\n", val1, val2, val1 / val2);
+        printf("%d * %d = %d\n", val1, val2, val1 * val2);
+    }
+}
