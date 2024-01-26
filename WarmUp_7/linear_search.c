05f9bc5545f8791de997ab2cded696cc6c19f4b9
Zarak Tariq <zaraktariq7@gmail.com>
2024-01-26 03:33:58 +0000

    WarmUp 7 submission

---
+++ WarmUp_7/linear_search.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int array[] = {1, 4, 7, 9, 12};
+    int target = 9;
+    int aLength = sizeof(array) / sizeof(array[0]);
+
+    int index = linearSearch(array, target, aLength);
+    if (index != -1) {
+        printf("Element found at index %d\n", index);
+    } else {
+        printf("Element not found :(\n");
+    }
+    
+
+}
+
+int linearSearch(int a[], int t, int length) {
+    for (int i = 0; i < length; i++) {
+        if (a[i] == t)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
